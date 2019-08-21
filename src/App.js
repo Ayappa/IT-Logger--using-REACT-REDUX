07a490f1069730ed460061de33bf3826a7a177a8
Ayappa Krishnappa <ayappaganesh1995@gmail.com>
2019-08-21 10:49:02 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, Fragment } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModel from "./components/techs/TechListModal";
+import {Provider} from 'react-redux';
+import  store from './store';
+
+const App = () => {
+	useEffect(() => {
+		M.AutoInit();
+	});
+	return (
+		<Provider store={store}>
+		<Fragment>
+			<SearchBar />
+      <div className="container">
+        <AddBtn/>
+        <AddLogModal/>
+		<EditLogModal/>
+		<AddTechModal/>
+		<TechListModel/> 
+        <Logs/>
+      </div>
+		</Fragment>
+		</Provider>
+	);
+};
+
+export default App;
